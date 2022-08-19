730624dbfe6ac440ce3b366acae8b34337bfb34e
Paulo Marcelo <marcelock3@gmail.com>
2022-08-19 13:18:52 -0300

    Desafio finalizado

--- Script.js
+++ Script.js
@@ -1,136 +1,115 @@
-let input1 = document.querySelector('.input1');
-let input2 = document.querySelector('.input2');
-let botao1 = document.querySelector('.botao1');
+// Declaração das lets:
+let inputNome = document.querySelector('.inputNome');
+let inputSelect = document.querySelector('.inputSelect');
+let inputDigite = document.querySelector('.inputDigite');
+let botao = document.querySelector('.botao');
 let fulano = document.querySelector('.fulano');
+let jogar = document.querySelector('.jogar');
+let reset = document.querySelector('.reset');
+let info = document.querySelector('.info');
 let quanTentativas = document.querySelector('.quanTentativas');
-let botao2 = document.querySelector('.botao2');
 let numSorteado;
+let contador = 3;
 
-input1.addEventListener('input', function(){
-    if (input1.value === "") {
-        botao1.disabled = true;
-        botao1.style.backgroundColor = '#B0A3A3';
+// Alterando a cor e habilitando o botão (começar) ao adicionar um nick:
+inputNome.addEventListener('input', function(){
+    if (inputNome.value == "") {
+        botao.disabled = true;
+        botao.style.backgroundColor = '#B0A3A3';
     }
     else {
-        botao1.disabled = false;
-        botao1.style.backgroundColor = '#1180E6';
+        botao.disabled = false;
+        botao.style.backgroundColor = '#1180E6';
     }
 });
 
-botao1.addEventListener('click', function(){
+// Habilitando os textos da tela e sorteando o numero a ser descoberto:
+botao.addEventListener('click', function(){
     quanTentativas.innerHTML = "Você tem 3 tentativas!"
-    fulano.innerHTML = input1.value;
+    fulano.innerHTML = inputNome.value;
+    jogar.disabled = false;
+    jogar.style.backgroundColor = '#F2890D';
+    botao.disabled = true;
+    botao.style.backgroundColor = '#B0A3A3';
     
-    let valor = input2.options[input2.selectedIndex].value;
+    let valor = inputSelect.options[inputSelect.selectedIndex].value;
     let min, max;
-    console.log(valor);
-})
-   /* switch (valor) {
-        case 'opcao1':
-            min = 1;
-            max = 10;
-            numSorteado = Math.floor(Math.random() * (max=min+1))+min;
-            console.log(numSorteado);
-           // return numSorteado
-        case 'opcao2':
-            min = 1;
-            max = 100;
-            numSorteado = Math.floor(Math.random() * (max=min+1))+min;
-            console.log(numSorteado);
-           // return numSorteado
-        case 'opcao3':
-            min = 1;
-            max = 200;
-            numSorteado = Math.floor(Math.random() * (max=min+1))+min;
-            console.log(numSorteado);
-          //  return numSorteado          
-        default:
-            break;
-    }
-});
 
-function sortear(){
-    let input2 = document.querySelector('.input2');
-    let valor = input2.options[input2.selectedIndex].value;
-    let min, max;
-    console.log(valor);
-
+    // Console com o valor:
+    // console.log(valor);
+    
     switch (valor) {
         case 'opcao1':
             min = 1;
             max = 10;
-            numSorteado = Math.floor(Math.random() * (max=min+1))+min;
+            numSorteado = Math.floor(Math.random() * (max - min + 1)) + min;
             console.log(numSorteado);
             return numSorteado
         case 'opcao2':
             min = 1;
             max = 100;
-            numSorteado = Math.floor(Math.random() * (max=min+1))+min;
+            numSorteado = Math.floor(Math.random() * (max - min + 1)) + min;
             console.log(numSorteado);
             return numSorteado
         case 'opcao3':
             min = 1;
             max = 200;
-            numSorteado = Math.floor(Math.random() * (max=min+1))+min;
+            numSorteado = Math.floor(Math.random() * (max - min + 1)) + min;
             console.log(numSorteado);
             return numSorteado          
         default:
             break;
     }
-    
-    const resetar = () => {
-        botao1.addEventListener('click', function () {
-            botao1.innerHTML = "RECOMEÇAR";
-            tentativas = 3;
-            text3.innerHTML = "";
-            quanTentativas.innerHTML = `Você ainda tem ${tentativas} tentativas`;
-            botao1.style.display = "initial";
-        })
-    }
-}
+});
 
+// Função sorteio do jogo ao selecionar o intervalo:
+function sortear(){
+    let inputSelect = document.querySelector('.inputSelect');       
+};
+
+// Função para comparar o valor digitado ao sorteado e os resultados:
 function conteudoSecundario(){
-    let input3 = parseInt (document.querySelector('.input3').value);
     let info = document.querySelector ('.info');
 
-    if (numero == numSorteado){
+    if (inputDigite.value == numSorteado){
         info.innerHTML = "Parabéns, você conseguiu adivinhar!";
-        botao2.disabled = true; 
-        const resetar = () => {
-            botao1.addEventListener('click', function () {
-                botao1.innerHTML = "RECOMEÇAR";
-                tentativas = 3;
-                text3.innerHTML = "";
-                quanTentativas.innerHTML = `Você ainda tem ${tentativas} tentativas`;
-                botao1.style.display = "initial";
-            })
-        }   
-    }
-    else if (numero > numSorteado){
+        jogar.disabled = true; 
+        jogar.style.backgroundColor = '#B0A3A3';
+        quanTentativas.innerHTML = "";
+        reset.style.display = "block";
+        jogar.style.display = "none";
+    }   
+    else if (inputDigite.value > numSorteado){
         info.innerHTML = "O número digitado é maior";
+        contador = contador - 1;
+        quanTentativas.innerHTML = `Você tem ${contador} tentativa(s)`;
     }
     else {
         info.innerHTML = "O número digitado é menor";
-        const resetar = () => {
-            botao1.addEventListener('click', function () {
-                botao1.innerHTML = "RECOMEÇAR";
-                tentativas = 3;
-                text3.innerHTML = "";
-                quanTentativas.innerHTML = `Você ainda tem ${tentativas} tentativas`;
-                botao1.style.display = "initial";
-            })
-        }
+        contador = contador - 1;
+        quanTentativas.innerHTML = `Você tem ${contador} tentativa(s)`;
     }
+    if (contador == 0) {
+        info.innerHTML = `Você perdeu! O número era ${numSorteado}.`;
+        quanTentativas.innerHTML = "";
+        jogar.disabled = true;
+        jogar.style.backgroundColor = '#B0A3A3';
+        reset.style.display = "block";
+        jogar.style.display = "none";   
+    }
+};
 
-botao2.addEventListener('click', conteudoSecundario) 
-}
+// Criando um botão "Recomeçar":
+reset.addEventListener('click', function(){
+    inputDigite.value = "";
+    quanTentativas.value = "";
+    jogar.style.display = "block";
+    reset.style.display = "none";
+    info.innerHTML = ""; 
+    contador = 3;    
+    botao.disabled = false;
+    botao.style.backgroundColor = '#1180E6';
+});
 
-const resetar = () => {
-    comecar.addEventListener('click', function () {
-        comecar.innerHTML = "COMEÇAR";
-        qntTentativas = 3;
-        saida.innerHTML = "";
-        tentativa.innerHTML = `Você ainda tem ${qntTentativas} tentativas`;
-        jogar.style.display = "initial";
-    })
-}*/
+// Jogar:
+jogar.addEventListener('click', conteudoSecundario); 
